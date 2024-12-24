9df09bb9a6980aa255020bf2b0ebe068c2ce5fe1
Rodrigo Passos <rodrigo@happycog.com>
2024-12-24 16:12:36 -0500

    Fix PHPStan issues

--- src/web/views/exception.php
+++ src/web/views/exception.php
@@ -1,6 +1,7 @@
 <?php
-/* @var $this \yii\web\View */
-/* @var $exception \Throwable */
-/* @var $handler \webrgp\ignition\web\ErrorHandler */
+
+/** @var \yii\web\View $this */
+/** @var \Throwable $exception */
+/** @var \webrgp\ignition\web\IgnitionErrorHandler $handler */
 
 $handler->renderIgnition($exception);
