b837458e1986e679b3eb8fcb7a0a0ee0f8f52769
Rodrigo Passos <rodrigo@happycog.com>
2024-12-27 15:55:12 -0500

    Add IgnitionRenderer service for enhanced exception handling and configuration management

---
+++ src/services/IgnitionRenderer.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace webrgp\ignition\services;
+
+use Craft;
+use craft\base\Component;
+use craft\helpers\App;
+use Spatie\FlareClient\Flare;
+use Spatie\FlareClient\FlareMiddleware\AddGitInformation;
+use Spatie\FlareClient\FlareMiddleware\CensorRequestBodyFields;
+use Spatie\FlareClient\FlareMiddleware\CensorRequestHeaders;
+use Spatie\Ignition\Config\IgnitionConfig;
+use Spatie\Ignition\Ignition as SpatieIgnition;
+use webrgp\ignition\Ignition;
+use webrgp\ignition\models\IgnitionSettings;
+use Throwable;
+use webrgp\ignition\middleware\AddCraftInfo;
+use webrgp\ignition\middleware\CraftSensitiveKeywords;
+use webrgp\ignition\middleware\RemoveAllRequestIp;
+
+class IgnitionRenderer extends Component
+{
+
+    private ?SpatieIgnition $ignition = null;
+
+    // Public Methods
+    // =========================================================================
+
+    public array $ignitionConfig = [];
+
+    public ?string $applicationPath = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        $this->ignitionConfig = $this->getIgnitionConfig();
+        $this->applicationPath = Craft::getAlias('@root');
+        $this->ignition = $this->initIgnition();
+
+        parent::init();
+    }
+
+    public function handleException(Throwable $exception): void
+    {
+        $this->ignition->handleException($exception);
+    }
+
+    /**
+     * Retrieves the Ignition configuration settings.
+     *
+     * This method collects various configuration settings for Ignition from the class properties
+     * or environment variables. It filters out any null values and returns the resulting array.
+     *
+     * @return array The Ignition configuration settings.
+     */
+    private function getIgnitionConfig(): array
+    {
+        $config = new IgnitionSettings([
+            'editor' => App::env('CRAFT_IGNITION_EDITOR') ?? null,
+            'theme' => App::env('CRAFT_IGNITION_THEME') ?? 'auto',
+            'remote_sites_path' => App::env('CRAFT_IGNITION_REMOTE_SITES_PATH') ?? null,
+            'local_sites_path' => App::env('CRAFT_IGNITION_LOCAL_SITES_PATH') ?? null,
+            'share_endpoint' => App::env('CRAFT_IGNITION_SHARE_ENDPOINT') ?? null,
+            'enable_share_button' => App::env('CRAFT_IGNITION_ENABLE_SHARE_BUTTON') ?? null,
+            'enable_runnable_solutions' => App::env('CRAFT_IGNITION_ENABLE_RUNNABLE_SOLUTIONS') ?? null,
+            'hide_solutions' => App::env('CRAFT_IGNITION_HIDE_SOLUTIONS') ?? null,
+        ]);
+
+        $config = array_filter($config->toArray(), fn($value) => $value !== null);
+
+        return $config;
+    }
+
+    /**
+     * Initializes and configures an Ignition instance.
+     *
+     * This method retrieves the Ignition configuration, creates an Ignition instance,
+     * and applies the configuration if available. It also sets the application path,
+     * determines whether exceptions should be displayed based on the development mode,
+     * and specifies that the application is not running in a production environment.
+     *
+     * @return Ignition The configured Ignition instance.
+     */
+    private function initIgnition(): SpatieIgnition
+    {
+        $ignition = SpatieIgnition::make();
+        $ignition->setConfig(new IgnitionConfig($this->ignitionConfig));
+
+        $middlewares = self::getFlareMiddlewares();
+
+        return $ignition
+            ->applicationPath($this->applicationPath)
+            ->shouldDisplayException(App::devMode())
+            ->runningInProductionEnvironment(false)
+            ->configureFlare(function (Flare $flare) use ($middlewares) {
+                $flare->registerMiddleware($middlewares);
+            });
+    }
+
+    private static function getFlareMiddlewares(): array
+    {
+        return [
+            new AddCraftInfo(),
+            new CensorRequestBodyFields(['password', 'password_confirmation']),
+            new CensorRequestHeaders([
+                'API-KEY',
+                'Authorization',
+                'Cookie',
+                'Set-Cookie',
+                'X-CSRF-TOKEN',
+                'X-XSRF-TOKEN',
+                // IP headers
+                'ip',
+                'x-forwarded-for',
+                'x-real-ip',
+                'x-request-ip',
+                'x-client-ip',
+                'cf-connecting-ip',
+                'fastly-client-ip',
+                'true-client-ip',
+                'forwarded',
+                'proxy-client-ip',
+                'wl-proxy-client-ip',
+            ]),
+            new CraftSensitiveKeywords(),
+        ];
+    }
+}
